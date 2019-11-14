0c00b6b633aa3e9045f1a36eead8e03f53a6f06a
ManSleen <mike.vansleen@gmail.com>
2019-11-13 23:32:02 -0700

    for_each and contains tests are passinggit add .git add .git add .

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,6 +2,7 @@
 # from dll_queue import Queue
 # import sys
 # sys.path.append('../queue_and_stack')
+import random
 
 
 class BinarySearchTree:
@@ -30,9 +31,21 @@ class BinarySearchTree:
     # False if it does not
 
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        elif target < self.value and self.left is None:
+            return False
+        elif target >= self.value and self.right is None:
+            return False
+        elif target < self.value and self.left is not None:
+            self = self.left
+            return self.contains(target)
+        elif target >= self.value and self.right is not None:
+            self = self.right
+            return self.contains(target)
 
     # Return the maximum value found in the tree
+
     def get_max(self):
         # Go right until you can't go right any more
         while self.right is not None:
@@ -45,14 +58,27 @@ class BinarySearchTree:
 
     def for_each(self, cb):
         # visit each node exactly one time
-        # go left until you cant anymore, then go back and go right
-        # somewhere in here call cb(node)
-        pass
+        # go left until you cant anymore, then go back to the first branch and go right, then go up to the next branch and repeat
 
+        # somewhere in here call cb(node)
+
+        if self.left is not None:
+            prev = self
+            self = self.left
+            self.for_each(cb)
+            self = prev
+        if self.right is not None:
+            prev = self
+            self = self.right
+            self.for_each(cb)
+            self = prev
+
+        return cb(self.value)
    # DAY 2 Project -----------------------
 
    # Print all the values in order from low to high
    # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
         pass
 
@@ -78,21 +104,35 @@ class BinarySearchTree:
         pass
 
 
+def add_two(node):
+    return node.value + 2
+
+
+arr = []
+
+
+def cb(x):
+    return arr.append(x)
+
+
+v1 = random.randint(1, 101)
+v2 = random.randint(1, 101)
+v3 = random.randint(1, 101)
+v4 = random.randint(1, 101)
+v5 = random.randint(1, 101)
+
 bst = BinarySearchTree(5)
 
+
+bst.insert(v1)
+bst.insert(v2)
+bst.insert(v3)
+bst.insert(v4)
+bst.insert(v5)
+
+
+bst.for_each(cb)
 print(bst)
 
-bst.insert(2)
-print(bst)
-bst.insert(3)
-print(bst)
-bst.insert(7)
-print(bst)
-bst.insert(6)
-print(bst)
-bst.insert(8)
-print(bst)
-bst.insert(10)
-print(bst)
-print("4 right: ", bst.right.right.right.right)
-print("get_max: ", bst.get_max())
+# print(bst.contains(6))
+print("arr: ", arr)
